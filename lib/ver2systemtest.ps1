90b9e68e2777e78ca48fd43bde79578d358d5432
Kazuhiro Nakamura <nakamura_k@agricom.co.jp>
2019-05-24 11:18:29 +0900

    パターン番号を6桁に変更、のついでに変更が簡単にできるようにリファクタリング。addしてなかった。

--- lib/ver2systemtest.ps1
+++ lib/ver2systemtest.ps1
@@ -1,18 +1,21 @@
+$__pattern_no_max_length = 6
+
 function pattern_no_is_valid($p_no) {
-    return $p_no -match "\d{1,6}"
+    return $p_no -match "\d{1,$($__pattern_no_max_length)}"
 }
 
 function read_pattern_no() {
     while($True) {
-        $pattern_no = read-host "テストパターン番号があれば入力してください。 (1～999999) "
+        $pattern_no = read-host "テストパターン番号があれば入力してください。 (1～$('9' * $__pattern_no_max_length)) "
         
         if ($pattern_no -eq "") {
-            Write-Host "パターン番号の入力を省略しました。パターン番号000001を使用します。"
-            return "P000001"
+            $pattern_no = '0' * ($__pattern_no_max_length - 1) + '1'
+            Write-Host "パターン番号の入力を省略しました。パターン番号$($pattern_no)を使用します。"
+            break
         }
         
-        $pattern_no = "00000$($pattern_no.trim())"
-        $pattern_no = $pattern_no.Substring($pattern_no.Length - 6, 6)
+        $pattern_no = '0' * ($__pattern_no_max_length - 1) + $($pattern_no.trim())
+        $pattern_no = $pattern_no.Substring($pattern_no.Length - $__pattern_no_max_length, $__pattern_no_max_length)
 
         
         if ($(pattern_no_is_valid $pattern_no) -eq $False) {
